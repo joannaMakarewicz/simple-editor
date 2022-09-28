f535f16d319e45924f2df665841bb92b78fbfb02
Joanna Makarewicz <joanna.milun@gmail.com>
2022-09-28 22:42:57 +0200

    history items added

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,31 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
 
 /* place your code below */
+import moment from "moment";
 
-const textarea = document.querySelector('.textarea--js');
-const save = document.querySelector('.save--js');
-const load = document.querySelector('.load--js');
+const mainDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+const dateTemporary = document.querySelector(".date--js");
+dateTemporary.innerHTML = mainDate;
 
-save.addEventListener('click', () => {
-    localStorage.setItem('information', textarea.value);
-}
-)
+const textarea = document.querySelector(".textarea--js");
+const save = document.querySelector(".save--js");
+const load = document.querySelector(".load--js");
+const historyDate = document.querySelector(".history__date--js");
+const historyContent = document.querySelector(".history__content--js");
 
-load.addEventListener('click', (e) => {
-    e.preventDefault();
-    textarea.value = localStorage.getItem('information');
-}
-)
+save.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.setItem("information", textarea.value);
+  localStorage.setItem("date", mainDate);
+  historyDate.innerHTML = localStorage.getItem("date");
+  historyContent.innerHTML = localStorage.getItem("information");
+});
+
+load.addEventListener("click", (e) => {
+  e.preventDefault();
+  textarea.value = localStorage.getItem("information");
+});
