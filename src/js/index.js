232a907b9b07e2b0871a56a4d4ae4e85cbae834c
Joanna Makarewicz <joanna.milun@gmail.com>
2022-12-02 14:16:04 +0100

    Date function in JS added

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,25 @@
 import "../scss/main.scss";
 
+console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
+
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
+const space = document.querySelector('.date--js')
 
-/* place your code below */
-import moment from "moment";
-
-const mainDate = moment().format("MMMM Do YYYY, h:mm:ss a");
-const dateTemporary = document.querySelector(".date--js");
-dateTemporary.innerHTML = mainDate;
-
-const textarea = document.querySelector(".textarea--js");
-const save = document.querySelector(".save--js");
-const load = document.querySelector(".load--js");
-const historyInformation = document.querySelector(".history__information--js");
-
-
-
-let tableInformation = [];
-console.log(tableInformation);
-
-if(informationFromLocalTable){
-for (let element of tableInformation){
-  historyInformation.innerHTML+=`<li>${element}</li>`}}else{
-  historyInformation.innerHTML=`Brak wpisów`;
+function myDate() {
+const date = new Date();
+const hour = date.getHours();
+const minute = date.getMinutes();
+const seconds = date.getSeconds();
+const year = date.getFullYear();
+const days = date.getDate();
+const month = date.getMonth();
+space.innerHTML=`${days}.${month}.${year}` + " " + `${hour}:${minute}:${seconds}`;
 }
+setInterval(myDate, 1000);
+myDate();
 
-save.addEventListener("click", (e) => {
-  e.preventDefault();
-  tableInformation.unshift(textarea.value);
-  console.log(tableInformation);
 
-  }
-)
 
-load.addEventListener("click", (e) => {
-  e.preventDefault();
-  textarea.value = localStorage.getItem("information");
-});
+
