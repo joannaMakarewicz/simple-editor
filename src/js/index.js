2defdba01dc77b2070dd94f8d9e4190436a33c1b
Joanna Makarewicz <joanna.milun@gmail.com>
2022-12-06 12:47:12 +0100

    JS - history of text after save

--- src/js/index.js
+++ src/js/index.js
@@ -5,21 +5,48 @@ console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
-const space = document.querySelector('.date--js')
+
+//current date&time function
+
+const space = document.querySelector(".date--js");
 
 function myDate() {
-const date = new Date();
-const hour = date.getHours();
-const minute = date.getMinutes();
-const seconds = date.getSeconds();
-const year = date.getFullYear();
-const days = date.getDate();
-const month = date.getMonth();
-space.innerHTML=`${days}.${month}.${year}` + " " + `${hour}:${minute}:${seconds}`;
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const seconds = date.getSeconds();
+  const year = date.getFullYear();
+  const days = date.getDate();
+  const month = date.getMonth();
+  space.innerHTML =
+    `${days}.${month}.${year}` + ", " + `${hour}:${minute}:${seconds}`;
 }
 setInterval(myDate, 1000);
 myDate();
 
+//Archive of text:
+
+const save = document.querySelector(".save--js");
+const myData = document.querySelector(".form__content--js");
+const myHistory = document.querySelector(".history__container--js");
+
+//check if sth is in localStorage and put into textarea if true:
+if (localStorage.getItem("myTextarea") !== null) {
+  myData.value = localStorage.getItem("myTextarea");
+}
+myData.addEventListener("input", (e) => {
+  localStorage.setItem("myTextarea", myData.value);
+});
 
 
+myHistory.innerHTML = localStorage.getItem("wholeHistory");
 
+
+save.addEventListener("click", (e) => {
+  e.preventDefault();
+  const actualHistory = `<img class="history__edit" src="../src/assets/icons/edit.svg" />
+  <ul class="history__list history__list--js">
+  <li class="history__information"><p class="history__information--date">${space.textContent}</p>${myData.value}</li></ul>`;
+  myHistory.innerHTML += actualHistory;
+  localStorage.setItem("wholeHistory", myHistory.innerHTML);
+});
